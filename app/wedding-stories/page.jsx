9e2f862ff44281960bfcde6eb5daf6d3886f95c8
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-03-10 12:40:10 -0400

    added wedding story skeleton

--- app/wedding-stories/page.jsx
+++ app/wedding-stories/page.jsx
@@ -1,10 +1,39 @@
+import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { getAllWeddingPosts } from "../utils/WeddingStories";
+
+export const metadata = {
+  title: "Wedding Stories - Bedge Pictures",
+  description: "Wedding Stories",
+};
 
 export default function WeddingStories() {
+  const weddingPosts = getAllWeddingPosts();
+
   return (
     <>
       <div className="hero">hello wedding stories</div>
+      <h1>Blog Posts</h1>
+      <ul>
+        {weddingPosts.map((weddingPost) => (
+          <li className="mb-5" key={weddingPost.slug}>
+            {/* <Image
+              src="https://bedgepictures.com/wp-content/uploads/2021/05/0B2A5303copy-scaled.jpg"
+              alt="picture"
+              width={50}
+              height={50}
+            /> */}
+            <Link
+              className="font-cormorant"
+              href={`/wedding-story/${weddingPost.slug}`}
+            >
+              {weddingPost.title}
+            </Link>
+            <p>{weddingPost.date}</p>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
